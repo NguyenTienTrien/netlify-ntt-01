3d7e8b7cb217232612c22f4553c9f711a94c748e
Nguyen Tien Trien <triennguyen@kargo365.com>
2022-03-20 19:45:40 +0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Page01 from "./page/page_01"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
-          Nguyen Tien Trien Demo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  /!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
+      {/*  <p>*/}
+      {/*    Nguyen Tien Trien Demo*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
+      <Page01/>
     </div>
   );
 }
---
+++ src/page/page_01.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class Page_01 extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                AAAAAAAAAAAAAA
+            </div>
+        )
+    }
+}
+export default Page_01;
